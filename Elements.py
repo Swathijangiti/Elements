795bbaebded7b17ede4510e59a91845711162660
Swathijangiti <70907467+Swathijangiti@users.noreply.github.com>
2020-09-07 22:15:19 +0530

    Elements.py
    Print the elements

---
+++ Elements.py
@@ -0,0 +1,23 @@
+ #assigning different elements to lists 
+list=['1','2','3']
+ list1=['4','5','6']
+ list.append('7')
+ Print(list)
+ Print(list1)
+ list.insert(2,'8')
+ Print(list)
+ list.remove('2')
+ Print(list)
+#accessing tuples
+tup=('1','2','3')
+Print(tup)
+tup2=('c','a','p','t','a','i','n')
+Print(tup2)
+Print(tup+tup2)
+
+
+ #deleting different elements from dictionaries
+myDic = {"AI","CSE","BTECH"}
+Print(myDict)
+del myDict["BTECH"]
+Print(myDict)
